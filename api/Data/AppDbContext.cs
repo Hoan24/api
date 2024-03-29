4d04eaec2db85893ae8587380c9b0864bbaa0131
Hoan24 <hoancitvn@gmail.com>
2024-03-29 20:41:31 +0700

    Add project files.

---
+++ api/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using api.Models;
+using static api.Models.Author;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+namespace api.Data
+{
+    public class AppDbContext:DbContext
+    {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+            }
+
+            public DbSet<Author> Authors { get; set; }
+            public DbSet<Book> Books { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder builder)
+            {
+
+                // Define relationship between books and authors
+                builder.Entity<Book>()
+                    .HasOne(x => x.Author)
+                    .WithMany(x => x.Books);
+
+                // Seed database with authors and books for demo
+                new DbInitializer(builder).Seed();
+            }
+        }
+    }
+
